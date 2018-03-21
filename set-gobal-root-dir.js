e86328ca239afaaef35d322e7e617bcf3f6a05d2
Daniel Santana <github@danielsantana.net>
2018-03-21 17:05:09 -0400

    adds hability to overwrite previous value

--- set-gobal-root-dir.js
+++ set-gobal-root-dir.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const { globalScope } = require('./lib/global-scope');
 
-const setGlobalRootDir = (value = '') => {
-  if (globalScope.__rootdir) return;
+const setGlobalRootDir = (value = '', configurable = false, overwrite = false) => {
+  if (globalScope.__rootdir && !overwrite) return globalScope.__rootdir;
 
   if (!value) value = process.cwd();
 
   fs.readdirSync(value);
 
-  Object.defineProperty(globalScope, '__rootdir', { value });
+  Object.defineProperty(globalScope, '__rootdir', { value, configurable });
+
+  return globalScope.__rootdir;
 };
 
 module.exports = { setGlobalRootDir };
